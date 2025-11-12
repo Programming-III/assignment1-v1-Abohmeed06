d6548e66e0bd0ff2944a8cad67aeced1a01efc73
Ahmed Hossam Abd elraouf <ahmed.hossam@student.giu-uni.de>
2025-11-12 15:21:44 +0200

    Refactor header guard and include directives

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,22 @@
-#ifndef ENCLOSURE_H
-#define ENCLOSURE_H
-
+#ifndef ENCLOSURE_H_INCLUDED
+#define ENCLOSURE_H_INCLUDED
 #include "Animal.h"
+#include <iostream>
+#include <string>
 
-//define enclosure class here 
+using namespace std;
 
-
-
-
-
-
-
-
+class Enclosure {
+    private:
+        Animal* animal;
+        int capacity;
+        int currentCount;
+    public:
+        Enclosure();
+        Enclosure(Animal* animal, int capacity, int currentCount);
+        ~Enclosure();
+        int getCapacity();
+        void addAnimal(Animal* a);
+        void displayAnimals();
+};
 #endif
